33a2a5448a57e18fd063dfa25b364f004999e13a
nickyzj <reg.zj@outlook.com>
2017-10-13 15:53:21 +0800

    end of Chapter 3 : Basic Express Website

--- express_website/app.js
+++ express_website/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Welcome'});
+    res.render('index', {title: 'Computer Not Working?'});
 });
 
 app.get('/about', function(req, res) {
